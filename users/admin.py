debaac81e97511db769fd5475957fb6c4cc27c70
Julien Garcia <julien.garcia-lp@laposte.net>
2022-08-08 20:06:42 +0200

    feat(user admin): admin for users

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("email", "first_name", "last_name", )
+    fields = ("email", ("first_name", "last_name",), "password", "phone_number", "role")
+    search_fields = ['email', ]
+    search_help_text = "User email"
+
+
+admin.site.register(User, UserAdmin)
