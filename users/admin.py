acb1f342373b4d0976b64fbdad6669a6554687b5
Julien Garcia <julien.garcia-lp@laposte.net>
2022-08-09 16:50:04 +0200

    style(user admin): change display style

--- users/admin.py
+++ users/admin.py
@@ -4,7 +4,7 @@ from users.models import User
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("email", "first_name", "last_name", )
+    list_display = ("full_name", "email", "role", "date_created", "date_updated")
     fields = ("email", ("first_name", "last_name",), "password", "phone_number", "role")
     search_fields = ['email', ]
     search_help_text = "User email"
