1f5f511a985a156927ab75c70bf3b0f62a07dd79
Julien Garcia <julien.garcia-lp@laposte.net>
2022-08-11 10:44:19 +0200

    feat(validate_phone): add possibility to control from serializer

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ValidationError
+from rest_framework.exceptions import ValidationError as ValidationErrorDRF
 
 
 def _is_phone_number_valid(phone_number: str) -> bool:
@@ -14,10 +15,12 @@ def _format_phone_number(phone_number: str) -> str:
     return formatted_phone_number
 
 
-def validate_phone_number(phone_number: str) -> str:
-    """ format and validate phone number"""
+def validate_phone_number(phone_number: str, is_from_serializer: bool = False) -> str:
+    """ format and validate phone number """
 
     phone_number = _format_phone_number(phone_number)
     if not _is_phone_number_valid(phone_number):
+        if is_from_serializer:
+            raise ValidationErrorDRF(f"ERROR (Phone number): |--> {phone_number} <--| is an invalide format.")
         raise ValidationError(f"ERROR (Phone number): |--> {phone_number} <--| is an invalide format.")
     return phone_number
