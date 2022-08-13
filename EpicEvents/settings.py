b15b162a335f3991160e2dde6c582480344aa0d7
Julien Garcia <julien.garcia-lp@laposte.net>
2022-08-13 11:30:23 +0200

    feat(settings): authorize simpleJWT

--- EpicEvents/settings.py
+++ EpicEvents/settings.py
@@ -31,9 +31,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'authentication',
     'users',
     'api',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',),
+}
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=240),
