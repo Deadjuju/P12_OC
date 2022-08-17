0886ea0a3c2b2d5b1e3f03986455d946d15b450e
Julien Garcia <julien.garcia-lp@laposte.net>
2022-08-17 16:28:00 +0200

    build(requirements): install django-filter

--- EpicEvents/settings.py
+++ EpicEvents/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    # 'django_filters',
     'authentication',
     'users',
     'api',
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',),
+    # 'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
 }
