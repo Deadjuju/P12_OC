e6a59962ba478f4df26178ad1d2f459e80b19cbc
Julien Garcia <julien.garcia-lp@laposte.net>
2022-08-23 11:20:29 +0200

    feat(filters): add OrderByMixin to sort responses

--- api/filters.py
+++ api/filters.py
@@ -1,12 +1,29 @@
 import django_filters.rest_framework as drf_filters
+from django.core.exceptions import FieldError
 
 from api.models import Client, Contract, Event
+
+
+class OrderByMixin(drf_filters.FilterSet):
+    """Sort by a given value (id, email, etc.)"""
+
+    sort_by = drf_filters.CharFilter(
+        method='filter_sort_by',
+    )
+
+    @classmethod
+    def filter_sort_by(cls, queryset, name, value):
+        values = value.lower().split(',')
+        try:
+            return queryset.order_by(*values)
+        except FieldError:
+            return queryset
 
 
 # -------------------------------- Client --------------------------------
 
 
-class ClientFilterSet(drf_filters.FilterSet):
+class ClientFilterSet(OrderByMixin, drf_filters.FilterSet):
     """Implements filters to be used with ClientViewset."""
 
     name = drf_filters.CharFilter(
@@ -27,12 +44,13 @@ class ClientFilterSet(drf_filters.FilterSet):
         fields = ['name',
                   'name_contains',
                   'email',
-                  'email_contains']
+                  'email_contains',
+                  'sort_by', ]
 
 
 # -------------------------------- Contract --------------------------------
 
-class ContractFilterSet(drf_filters.FilterSet):
+class ContractFilterSet(OrderByMixin, drf_filters.FilterSet):
     """Implements filters to be used with ContractViewset."""
 
     client = drf_filters.CharFilter(
@@ -64,12 +82,13 @@ class ContractFilterSet(drf_filters.FilterSet):
                   'amount',
                   'min_amount',
                   'max_amount',
-                  'date']
+                  'date',
+                  'sort_by', ]
 
 
 # -------------------------------- Event --------------------------------
 
-class EventFilterSet(drf_filters.FilterSet):
+class EventFilterSet(OrderByMixin, drf_filters.FilterSet):
     """Implements filters to be used with EventViewset."""
 
     client = drf_filters.CharFilter(
@@ -99,4 +118,5 @@ class EventFilterSet(drf_filters.FilterSet):
                   'client_email_contains',
                   'event_date',
                   'min_event_date',
-                  'max_event_date']
+                  'max_event_date',
+                  'sort_by', ]
